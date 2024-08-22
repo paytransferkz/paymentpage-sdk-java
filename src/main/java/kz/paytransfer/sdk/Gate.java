848de326ff2879eb3a33b09f770e9ec024a615f6
paytransferkz <developers@paytransfer.kz>
2024-08-22 20:02:16 +0500

    initial

---
+++ src/main/java/kz/paytransfer/sdk/Gate.java
@@ -0,0 +1,56 @@
+package kz.paytransfer.sdk;
+
+/**
+ * Class for communicate with our
+ */
+public class Gate
+{
+    /**
+     * kz.paytransfer.sdk.SignatureHandler instance for check signature
+     */
+    private SignatureHandler signatureHandler;
+
+    /**
+     * kz.paytransfer.sdk.PaymentPage instance for build payment URL
+     */
+    private PaymentPage paymentPageUrlBuilder;
+
+    /**
+     * kz.paytransfer.sdk.Gate constructor
+     * @param secret site salt
+     */
+    public Gate(String secret) {
+        signatureHandler = new SignatureHandler(secret);
+        paymentPageUrlBuilder = new PaymentPage(signatureHandler);
+    }
+
+    /**
+     * Method for set base payment page URL
+     * @param url payment page URL
+     * @return self for fluent interface
+     */
+    public Gate setBaseUrl(String url) {
+        paymentPageUrlBuilder.setBaseUrl(url);
+
+        return this;
+    }
+
+    /**
+     * Method build payment URL
+     * @param payment kz.paytransfer.sdk.Payment instance with payment params
+     * @return string URL that you can use for redirect on payment page
+     */
+    public String getPurchasePaymentPageUrl(Payment payment) {
+        return paymentPageUrlBuilder.getUrl(payment);
+    }
+
+    /**
+     * Method for handling callback
+     * @param data raw callback data in JSON format
+     * @return kz.paytransfer.sdk.Callback instance
+     * @throws ProcessException throws when signature is invalid
+     */
+    public Callback handleCallback(String data) throws ProcessException {
+        return new Callback(data, signatureHandler);
+    }
+}
